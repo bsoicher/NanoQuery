d413e37747e5ff92aacf3fdf188230ae1d6302a6
Ben Soicher <ben.soicher@gmail.com>
2015-05-29 13:43:25 -0400

    Create nano.js
    Initial commit

---
+++ nano.js
@@ -0,0 +1,37 @@
+function $(selector) {
+    return (this instanceof $) ? this.find(selector) : new $(selector);
+}
+
+$.prototype = {
+    
+    find: function(selector) {
+        this.a = [].slice.call(document.querySelectorAll(selector));
+        return this;
+    },
+
+    each: function(func) {
+        for (var i = 0, l = this.a.length; i < l; i++) {
+            func(this.a[i]);
+        }
+        return this;
+    },
+
+    css: function(name, value) {
+        return this.each(function(e) {
+            e.style.setProperty(name, value, null);
+        });
+    },
+
+    attr: function(name, value) {
+        return this.each(function(e) {
+            e.setAttribute(name, value);
+        });
+    },
+
+    on: function(name, func) {
+        return this.each(function(e) {
+            e.addEventListener(name, func);
+        });
+    }
+
+};
