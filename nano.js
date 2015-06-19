ab707cd197d9874423da93890294f986d5c9c12b
Ben Soicher <ben.soicher@gmail.com>
2015-06-19 11:53:27 -0400

    added comments

--- nano.js
+++ nano.js
@@ -1,3 +1,4 @@
+// Creates new instance and takes in a selector or element object
 function $(selector) {
     if (this instanceof $){
         this.a = selector && selector.nodeType ? [selector] : [].slice.call(document.querySelectorAll(selector));
@@ -8,24 +9,28 @@ function $(selector) {
 
 $.prototype = {
     
+    // Itterate through array.  Could be done using [].forEach, but this should perform better
     each: function(func) {
         for (var i = this.a.length; i--;)
             func(this.a[i]);
         return this;
     },
 
+    // Get/Set inline style value
     css: function(prop, value) {
         return value ? this.each(function(e) {
             e.style[prop] = value;
         }) : this.a[0].style[prop];
     },
-
+    
+    // Get/Set attribute value
     attr: function(key, value) {
         return value ? this.each(function(e) {
             e.setAttribute(key, value);
         }) : this.a[0].getAttribute(key);
     },
 
+    // Add event listeners
     on: function(type, callback) {
         return this.each(function(e) {
             e.addEventListener(type, callback);
